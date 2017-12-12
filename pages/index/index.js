98447300b4e72ba9cb169191297fb45168ae6091
zpc7 <zpc.excel@foxmail.com>
2017-12-12 23:43:16 +0800

    完成首页的卡片样式布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,23 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    data:{
+        cardList: [{
+                id: 1,                   //卡片
+                title:'春节',            //标题
+                type:'will',             //past已经过去  will还有
+                days:67,                 //距离的天数
+                time:'2018-02-15 星期四'  //目标日期或起始日期
+            },{
+                id: 2,
+                title:'毕业',
+                type:'past',
+                days:470,
+                time:'2016-07-01 星期五'
+            }]
+    },
+    //新增事件
+    addEvent(e){
+        //TODO
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
